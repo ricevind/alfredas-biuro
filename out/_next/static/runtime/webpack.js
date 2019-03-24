3f97b58479a9c660be02225aa8494f1a4e7b2b85
Ricevind <ricevind@gmail.com>
2019-03-24 19:42:59 +0100

    Deploy Next.js to gh-pages

--- out/_next/static/runtime/webpack.js
+++ out/_next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1b2c939b0a7d17cfda13";
+/******/ 	var hotCurrentHash = "20885653cbfa14203f29";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
